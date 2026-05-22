3380a7055e06a16a10b3bab8e549a16a1dd8c17b
joseph200536 <joseph200536@gmail.com>
2026-05-22 23:25:57 +0530

    online host

--- app.js
+++ app.js
@@ -11,8 +11,20 @@ const path = require('path');
 const app = express();
 
 // Middleware
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://rtca-frontend.vercel.app',
+    process.env.FRONTEND_URL
+].filter(Boolean);
+
 app.use(cors({
-    origin: 'http://localhost:5173', // Vite default port
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1 || origin.startsWith('http://localhost:')) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true
 }));
 app.use(express.json());
--- server.js
+++ server.js
@@ -12,9 +12,21 @@ const server = http.createServer(app);
 
 // Initialize Socket.io (can be used later)
 const { Server } = require('socket.io');
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://rtca-frontend.vercel.app',
+    process.env.FRONTEND_URL
+].filter(Boolean);
+
 const io = new Server(server, { 
     cors: { 
-        origin: 'http://localhost:5173',
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.indexOf(origin) !== -1 || origin.startsWith('http://localhost:')) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
         methods: ["GET", "POST"],
         credentials: true
     } 
